fc685ecb7f1e8d6bc3aba3fecbe6ce5567850c31
Tyler <pannelltyler91@gmail.com>
2024-04-06 09:22:54 +0900

    working on rewards functionality

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -2,36 +2,54 @@ import Button from "react-bootstrap/esm/Button";
 import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
+import Card from 'react-bootstrap/Card';
+import { useDispatch,useSelector } from "react-redux";
+import {addPhnNmbr,clearPhnNmbr,deletePhnNmbrDigit,checkRewards} from '../features/cart';
+import { useNavigate } from "react-router-dom";
 
 function Rewards() {
+
+    const dispatch = useDispatch();
+    const phoneNumber = useSelector((state) => state.cart.phnNmbr)
+    const navigate = useNavigate();
+
+  
     return (
         <Container >
             <h1>Rewards</h1>
-            <Container >
+            <p style={{marginBottom:'5px'}}>Please enter your phone number to gain/use rewards!</p>
+            <Container style={{margin:'5px',width:'75%'}} >
                 <Row>
-                    <Col>1</Col>
-                    <Col>2</Col>
-                    <Col>3</Col>
+                    <Card style={{margin:'5px'}}>
+                    {phoneNumber}
+                    </Card>
+                    
                 </Row>
                 <Row>
-                    <Col>4</Col>
-                    <Col>5</Col>
-                    <Col>6</Col>
+                    <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='1' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>1</Button></Col>
+                    <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='2' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>2</Button></Col>
+                    <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='3' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>3</Button></Col>
+                    
                 </Row>
                 <Row>
-                    <Col>7</Col>
-                    <Col>8</Col>
-                    <Col>9</Col>
+                <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='4' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>4</Button></Col>
+                <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='5' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>5</Button></Col>
+                <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='6' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>6</Button></Col>
                 </Row>
                 <Row>
-                    <Col><Button>Continue</Button></Col>
-                    <Col>0</Col>
-                    <Col><Button>Delete</Button></Col>
+                <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='7' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>7</Button></Col>
+                <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='8' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>8</Button></Col>
+                <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='9' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>9</Button></Col>
+                </Row>
+                <Row>
+                    <Col style={{border:'white solid 1px',backgroundColor:'#A47C6F',margin:'10px',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={() => {dispatch(clearPhnNmbr({message:"cleared"}))}} style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>Clear</Button></Col>
+                    <Col  style={{border:'white solid 1px',margin:'10px',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button onClick={(e) => {dispatch(addPhnNmbr({digit:e.target.value}))}} value='0' style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}>0</Button></Col>
+                    <Col style={{border:'white solid 1px',backgroundColor:'#A47C6F',margin:'10px',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}} onClick={(e) => {dispatch(deletePhnNmbrDigit({message:'delete'}))}}>Delete</Button></Col>
                 </Row>
             </Container>
             <Row>
                 <Col></Col>
-                <Col><Button href='/pay'>Continue</Button></Col>
+                <Col style={{border:'white solid 1px',backgroundColor:'#A47C6F',marginRight:'162px',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}}><Button  style={{border:'none',backgroundColor:'#A47C6F',borderRadius:'25px',display:'flex',justifyContent:'center',height:'100%',width:'100%'}} onClick={(e) =>{dispatch(checkRewards({message:'can we update?'}))}}>Continue</Button></Col>
                 <Col></Col>
                 </Row>
         </Container>
--- src/features/cart.js
+++ src/features/cart.js
@@ -1,8 +1,10 @@
 import {createSlice} from '@reduxjs/toolkit'
+import {addDoc,getDocs,updateDoc,collection,doc} from 'firebase/firestore';
+import {db} from '../components/config/firebase';
 
 export const cartSlice = createSlice({
     name:'cart',
-    initialState:{value:[],count:0,toGo:false,total:0},
+    initialState:{value:[],count:0,toGo:false,total:0,phnNmbr:'',userExists:false},
     reducers:{
         addToCart: (state, action) => {
                 console.log(action.payload)
@@ -15,6 +17,37 @@ export const cartSlice = createSlice({
         deleteCartItem: (state, action) => {
             
         },
+        addPhnNmbr: (state, action) => {
+            state.phnNmbr += action.payload.digit;
+            
+        },
+        clearPhnNmbr: (state, action) => {
+            state.phnNmbr = '';
+        },
+        deletePhnNmbrDigit: (state, action) => {
+            const newPhnNmbr= state.phnNmbr.slice(0,-1)
+            state.phnNmbr = newPhnNmbr;
+            console.log(state.phnNmbr)
+        },
+        checkRewards: async (state, action) => {
+            console.log(action.payload)
+            const usersCollectionRef = collection(db,"users")
+                const usersData = await getDocs(usersCollectionRef);
+                const filteredUsersData = usersData.docs.map((doc) => ({...doc.data(),id:doc.id,}));
+                console.log(filteredUsersData)
+                const foundUser = filteredUsersData.find((user) => user.phn_nmbr == state.phnNmbr)
+                if(foundUser){
+                    
+                }else{
+
+                }
+                
+            
+            
+        },
+        createRewards: (state, action) => {
+            
+        },
         hereToGo: (state,action) => {
             action.payload.toGo === true? localStorage.setItem('toGo',true) : localStorage.setItem('toGo',false)
             
@@ -24,5 +57,5 @@ export const cartSlice = createSlice({
     }
 })
 
-export const {addToCart,deleteCartItem,hereToGo} = cartSlice.actions;
+export const {addToCart,deleteCartItem,hereToGo,addPhnNmbr,clearPhnNmbr,deletePhnNmbrDigit,checkRewards} = cartSlice.actions;
 export default cartSlice.reducer;
