305a004f30525eafe585ea20631f142f8c5bb04c
Tyler <pannelltyler91@gmail.com>
2024-04-24 12:09:47 +0900

    added hot products to db and ui

--- src/components/menu_components/HotCoffe.js
+++ src/components/menu_components/HotCoffe.js
@@ -2,22 +2,42 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
-import { useSelector } from 'react-redux';
+import {db} from '../config/firebase';
+import {getDocs,collection} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import HotOptions from './HotOptionsOffCanvas';
 
-function HotCoffee(){
-    const icedCoffeeMenu = useSelector((state) => state.hotcoffee.value)
+function IcedCoffee(){
+    const [menu,setMenu] = useState([]);
+
+    useEffect(() =>{
+        const getMenu = async () =>{
+            try{
+                const menuCollectionRef = collection(db,"menu")
+                const menuData = await getDocs(menuCollectionRef);
+                const filteredMenuData = menuData.docs.map((doc) => ({...doc.data(),id:doc.id,}));
+                const icedFilteredMenuData = filteredMenuData.filter((coffee) => coffee.type === 'hot')
+                setMenu(icedFilteredMenuData)
+                
+                
+            }catch(err) {
+                console.log(err);
+            }
+        }
+        getMenu();
+    }, []);
+
     return(
         <Container>
             <Row>
-                {icedCoffeeMenu.map((coffee) => {
+                {menu.map((coffee) => {
                     return(
                         <Col md={6} key={coffee.id}>
                             <Card>
-                            <Card.Title>{coffee.name}</Card.Title>
+                            <Card.Title>{coffee.item}</Card.Title>
                             <Card.Body>
-                                <Card.Text>Price:{coffee.price}</Card.Text>
-                                <HotOptions/>
+                                <Card.Text>Price:${coffee.price}</Card.Text>
+                                <HotOptions coffeeName={coffee.item}  coffeeId={coffee.id} coffeePrice={coffee.price} />
                             </Card.Body>
                         </Card>
                         </Col>
@@ -28,4 +48,4 @@ function HotCoffee(){
     )
 }
 
-export default HotCoffee;
+export default IcedCoffee;
--- src/components/menu_components/HotOptionsOffCanvas.js
+++ src/components/menu_components/HotOptionsOffCanvas.js
@@ -4,12 +4,16 @@ import Row from 'react-bootstrap/Row';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { addToCart } from '../../features/cart';
+import { useDispatch } from 'react-redux';
+import Form from 'react-bootstrap/Form'
 
-function HotOptions() {
+function HotOptions({ coffeeName, coffeeId, coffeePrice }) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const dispatch = useDispatch();
 
     return (
         <>
@@ -21,34 +25,44 @@ function HotOptions() {
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Options</Offcanvas.Title>
                 </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <Row>
-                    <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                            <ToggleButton id="tbg-radio-2" value={2}>
-                                More Sweet
-                            </ToggleButton>
-                            <ToggleButton id="tbg-radio-3" value={3}>
-                                Less Sweet
-                            </ToggleButton>
-                        </ToggleButtonGroup>
-                    </Row>
-                    <br/>
-                    <Row>
-                        <ToggleButtonGroup type="radio" name="options3" defaultValue={6}>
-                            <ToggleButton id="tbg-radio-6" value={6}>
-                                Add a Shot
-                            </ToggleButton>
-                            <ToggleButton id="tbg-radio7" value={7}>
-                               Decaffeine
-                            </ToggleButton>
-                        </ToggleButtonGroup>
-                    </Row>
-                    <br/>
-                    <Button>Add to Cart</Button>
-                </Offcanvas.Body>
+                <Form onSubmit={(e) => { e.preventDefault(); dispatch(addToCart({ item: coffeeName, coffeeId: coffeeId + Math.random() * 10, coffeePrice: coffeePrice, sweet: e.target.options.value, shot: e.target.options3.value, toGo: localStorage.getItem('toGo') })) }}>
+                    <Offcanvas.Body>
+                        <Row>
+                            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                                <ToggleButton id="tbg-radio-2" value='More Sweet'>
+                                    More Sweet
+                                </ToggleButton>
+                                <ToggleButton id="tbg-radio-3" value='Less Sweet'>
+                                    Less Sweet
+                                </ToggleButton>
+                                <ToggleButton id="tbg-radio-3-2" value='Regular'>
+                                    Regular
+                                </ToggleButton>
+                            </ToggleButtonGroup>
+                        </Row>
+                        <br />
+                        <Row>
+                            <ToggleButtonGroup type="radio" name="options3" defaultValue={6}>
+                                <ToggleButton id="tbg-radio-6" value="Add Shot">
+                                    Add a Shot
+                                </ToggleButton>
+                                <ToggleButton id="tbg-radio7" value='Decaffeine'>
+                                    Decaffeine
+                                </ToggleButton>
+                                <ToggleButton id="tbg-radio7-2" value='Regular'>
+                                    Regular
+                                </ToggleButton>
+                            </ToggleButtonGroup>
+                        </Row>
+                        <br />
+                        <Button type='submit' onClick={handleClose}>Add to Cart</Button>
+                    </Offcanvas.Body>
+                </Form>
             </Offcanvas>
         </>
     );
 }
+
+
 
 export default HotOptions;
